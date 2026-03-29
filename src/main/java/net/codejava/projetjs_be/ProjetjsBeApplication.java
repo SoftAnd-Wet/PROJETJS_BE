f4ca167ff1d622b22f099b92b744ed062e8d0e2a
SoftAnd-Wet <starcrusherjonathan2nd@gmail.com>
2026-03-29 19:11:00 +0100

    initial commit

---
+++ src/main/java/net/codejava/projetjs_be/ProjetjsBeApplication.java
@@ -0,0 +1,13 @@
+package net.codejava.projetjs_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetjsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetjsBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/codejava/projetjs_be/ProjetjsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.codejava.projetjs_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetjsBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
